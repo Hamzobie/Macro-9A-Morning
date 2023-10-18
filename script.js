0da350e7234775b70ddf46db5d33f2d1ce80b663
Hamdani Zulva <hamdanizulva0@webmail.umm.ac.id>
2023-10-18 22:23:53 +0700

    new file:   DaftarUser.html 	new file:   LandingPage.html 	new file:   Login.html 	new file:   ProfilePageUMKM.html 	new file:   ProfilePageUser.html 	new file:   SandiBerubah.html 	new file:   SudahLogin.html 	new file:   UbahSandi.html 	new file:   artikel/artikel(all).html 	new file:   artikel/artikel1.html 	new file:   artikel/artikel2.html 	new file:   artikel/artikel3.html 	new file:   artikel/artikel4.html 	new file:   daftar-umkm/daftarumkm.html 	new file:   daftar-umkm/script.js 	new file:   detailumkm.html 	new file:   img/Facebook.png 	new file:   img/LoginVector.png 	new file:   img/ProfilePic.png 	new file:   img/bannerumkm.jpg 	new file:   img/bisnis-retail-scaled 2.jpg 	new file:   img/bisnis-retail-scaled 3.jpg 	new file:   img/bisnis-retail-scaled.jpg 	new file:   img/card1.png 	new file:   img/card2.png 	new file:   img/centang.png 	new file:   img/fb.png 	new file:   img/foto1.png 	new file:   img/foto3.png 	new file:   img/foto4.png 	new file:   img/fotoatasartikel.jpg 	new file:   img/fotokacang2.jpg 	new file:   img/fotokacang333.jpg 	new file:   img/fotoprofil.png 	new file:   img/fotoumkm1111.jpg 	new file:   img/google.png 	new file:   img/ikon1.png 	new file:   img/ikon2.png 	new file:   img/img_header1.png 	new file:   img/img_header2.png 	new file:   img/img_header3.png 	new file:   img/list1.png 	new file:   img/list2.png 	new file:   img/list3.png 	new file:   img/list4.png 	new file:   img/list5.png 	new file:   img/list6.png 	new file:   img/login.png 	new file:   img/logo.jpg 	new file:   img/placehorder_tambahProduk.png 	new file:   img/rotikacang44.png 	new file:   img/shoope.png 	new file:   img/tokopedia.png 	new file:   img/twitter.png 	new file:   img/twt.png 	new file:   img/umkm1.jpeg 	new file:   img/umkm2.jpeg 	new file:   img/umkm3.png 	new file:   img/umkm4-1.png 	new file:   img/umkm4-2.png 	new file:   img/umkm4-3.png 	new file:   img/umkm4.png 	new file:   img/welkom.png 	new file:   img/zyro-image.jpg 	new file:   list-umkm/list_umkm.html 	new file:   script.js 	new file:   style.css 	new file:   tambahProduk.html 	new file:   welcome/welcoming.html

---
+++ script.js
@@ -0,0 +1,52 @@
+const users = [{username : "User1", password : "User1", role: "user"},
+            {username :'umkm', password: 'umkm', role: "umkm"}];
+
+const roles = {
+    role : "user",
+    get getRole(){
+        return this.role;
+    },
+    set setRole(theRole){
+        this.role = theRole;
+    }
+};
+
+function authLogin() {
+    let username = document.getElementById("EmailUser").value;
+    let password = document.getElementById("PasswordUser").value; 
+    for (let i = 0; i < users.length; i++) {
+        if(username == users[i].username && password == users[i].password) {
+          roles.setRole = users[i].role;
+          window.location.href = 'SudahLogin.html';
+        }
+      }
+   alert('You are trying to break in!');
+}
+
+function authRole(){
+    if (roles.getRole == "umkm") {
+        window.location.href = 'ProfilePageUMKM.html';
+    } else {
+        window.location.href = 'ProfilePageUser.html';
+    }
+}
+
+document.getElementById('addImageLink').addEventListener('click', function (event) {
+    event.preventDefault();
+    var fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/*'; 
+    fileInput.addEventListener('change', function () {
+        var file = fileInput.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var newImage = document.createElement('img');
+                newImage.src = e.target.result;
+                document.querySelector('.image-container').appendChild(newImage);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+    fileInput.click();
+});
